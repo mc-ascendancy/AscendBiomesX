e67ffe8064858656ef8fa4cc04e103e603450043
Harry Billings <harry@froogo.co.uk>
2020-12-23 20:23:18 +0000

    Added biome configuration inheritance
    See #2 for more details. Closes #2
    
    Also updated POM version to v0.3.1.

--- src/main/java/com/ascendancyproject/ascendbiomes/CustomBiome.java
+++ src/main/java/com/ascendancyproject/ascendbiomes/CustomBiome.java
@@ -6,18 +6,37 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CustomBiome {
+    private boolean initialised;
+    private String inherit;
+
     private HashMap<String, Float> cropGrowthRates;
     private HashMap<String, Float> mobGrowthRates;
     private ArrayList<CustomEffect> statusEffects;
 
     public void inherit() {
-        // TODO: see issue #2.
+        if (initialised)
+            return;
+
+        initialised = true;
 
         if (cropGrowthRates == null)
             cropGrowthRates = new HashMap<>();
 
         if (mobGrowthRates == null)
             mobGrowthRates = new HashMap<>();
+
+        if (inherit != null) {
+            CustomBiome parent = Config.getInstance().getCustomBiomes().get(inherit);
+
+            if (!parent.initialised)
+                parent.inherit();
+
+            parent.cropGrowthRates.forEach((k, v) -> cropGrowthRates.putIfAbsent(k, v));
+            parent.mobGrowthRates.forEach((k, v) -> cropGrowthRates.putIfAbsent(k, v));
+
+            if (statusEffects == null)
+                statusEffects = parent.statusEffects;
+        }
 
         if (statusEffects == null)
             statusEffects = new ArrayList<>();
